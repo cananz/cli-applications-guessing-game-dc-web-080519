a597d8bc49f40c6667c2a7ae44d58c7193e39d49
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-08-02 19:31:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,8 @@
 # Code your solution here!
 
 def run_guessing_game
-  mind = rand(1..6)
+  mind = rand(6) + 1
+  puts "Read my mind! What number am I thinking of?"
   guess = gets.chomp
   
   if guess == mind.to_s
